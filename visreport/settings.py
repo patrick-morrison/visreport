a8d355ec91740e1952261c53a7a0b24eaccdc193
Patrick Morrison <21970531@student.uwa.edu.au>
2020-01-31 02:22:18 +0800

    map setup

--- visreport/settings.py
+++ visreport/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'observations.apps.ObservationsConfig',
     'accounts.apps.AccountsConfig',
+    'leaflet',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
+LEAFLET_CONFIG = {
+  'TILES': 'https://api.mapbox.com/styles/v1/patrickmorrison1498/ck603l62s0mbi1io55kc8hif5/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGF0cmlja21vcnJpc29uMTQ5OCIsImEiOiJjazNoNDQ4dG4wMmppM21ya3BtbWc5am9iIn0.nLex7QBehGELoOf7j9OqHg',
+  'SPATIAL_EXTENT': (115.1,-33,116.3,-31),
+  'DEFAULT_CENTER': (-32.00,115.70),
+  'DEFAULT_ZOOM': 10,
+  'MIN_ZOOM': 9,
+  'MAX_ZOOM': 12,
+}
