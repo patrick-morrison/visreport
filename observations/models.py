1bb3924c6f4218854e1cd9aebb79afe8387dc804
Patrick Morrison <patrick.morrison1498@gmail.com>
2020-02-02 17:17:16 +0800

    multiple observations on main map (slow)

--- observations/models.py
+++ observations/models.py
@@ -14,14 +14,18 @@ class Site(models.Model):
         return self.site_name + " (" + self.site_code + ")"
         
     def current_vis(self):
-        site = Observation.objects.filter(site=self.site_code)
-        return site.latest('when_observed').visibility
-
-    def last_updated(self):
-        site = Observation.objects.filter(site=self.site_code)
-        return site.latest('when_observed').when_observed
-        
-
+        site = Observation.objects.filter(site=self.site_code).order_by('-when_observed')
+        try:
+            return {"obs1": {"Date":str(site[0].when_observed)[:16],"Vis":site[0].visibility}, "obs2": {"Date":str(site[1].when_observed)[:16],"Vis":site[1].visibility}, "obs3": {"Date":str(site[0].when_observed)[:16],"Vis":site[0].visibility}}
+        except IndexError:
+            try:
+                return {"obs1": {"Date":str(site[0].when_observed)[:16],"Vis":site[0].visibility}, "obs2": {"Date":"No data","Vis":"No data"}, "obs3": {"Date":"No data","Vis":"No data"}}
+            except IndexError:
+                try:
+                    return {"obs1": {"Date":str(site[0].when_observed)[:16],"Vis":site[0].visibility}, "obs2": {"Date":"No data","Vis":"No data"}, "obs3": {"Date":"No data","Vis":"No data"}}
+                except IndexError:
+                    return "No observations"
+                
 class Observation(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     when_observed = models.DateTimeField()
@@ -32,3 +36,4 @@ class Observation(models.Model):
 
     def __str__(self):
         return str(self.when_observed) +"_"+ self.site.site_name + "_by_"+ self.user.username
+    
