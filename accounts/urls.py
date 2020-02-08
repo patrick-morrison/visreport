6ea1ec685ad10b834e2df64b898cf89d922518ad
Patrick Morrison <patrick.morrison1498@gmail.com>
2020-02-08 13:40:04 +0800

    email verification

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup', views.signup, name = "signup"),
     path('logout', views.logout, name = "logout"),
     path('profile', views.profile, name = "profile"),
+    path('activate/<uidb64>/<token>', views.ActivateAccountView.as_view(), name='activate')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,17 @@ from django.shortcuts import render
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.views import generic
 from django.contrib import auth
+from observations.models import Site, Observation
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
+from .utils import generate_token
+from django.core.mail import EmailMessage
+from django.conf import settings
+
 
 # Create your views here.
 
@@ -15,7 +25,7 @@ def login(request):
             auth.login(request, user)
             return redirect('map')
         else:
-             return render(request, 'accounts/login.html', {'error': "Username or password is incorrect"})
+             return render(request, 'accounts/login.html', {'error': "Username or password is incorrect, or user is not activated."})
     else:
         return render(request, 'accounts/login.html')
 
@@ -27,15 +37,44 @@ def signup(request):
                 user = User.objects.get(username = request.POST['username'])
                 return render(request, 'accounts/signup.html', {'error': "Username has already been taken"})
             except User.DoesNotExist: 
-                user = User.objects.create_user(request.POST['username'], password =request.POST['password1'], email =request.POST['email'])
-                auth.login(request,user)
-                return redirect('map')
+                user = User.objects.create_user(request.POST['username'], password =request.POST['password1'], email =request.POST['email'], is_active = False)
+                user.save()
+                current_site=get_current_site(request)
+                message=render_to_string('accounts/activate.html',
+                {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': generate_token.make_token(user)
+                }
+                )
+                email_message=EmailMessage(
+                    'Activate your Vis.Report Account',
+                    message,
+                    settings.EMAIL_HOST_USER,
+                    [user.email]
+                )
+                email_message.send()
+                return render(request, 'accounts/login.html', {'error': "Check your email for an activation link."})
         else:
             return render(request, 'accounts/signup.html', {'error': "Passwords must match"})
     else:
         return render(request, 'accounts/signup.html')
 
-    
+class ActivateAccountView(generic.View):
+    def get(self, request, uidb64, token):
+        try:
+            uid=force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except Exception as identifier:
+            user=None
+        if user is not None and generate_token.check_token(user,token):
+            user.is_active=True
+            user.save()
+            auth.login(request, user)
+            return redirect('map')
+        return render(request, 'accounts/login.html', {'error': "Could not activate, please request the activation link again."})
+
 def logout(request):
     if request.method == 'POST':
         auth.logout(request)
@@ -43,4 +82,6 @@ def logout(request):
 
         
 def profile(request):
-        return render(request, 'accounts/profile.html')
+        user = request.user
+        observations = Observation.objects.all().filter(user=user).order_by('-when_observed')
+        return render(request, 'accounts/profile.html', {"Observations":observations})
